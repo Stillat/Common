031f894747d4a72d37dfdb078b680115f9de65e4
Johnathon Koster <johnathon.koster@paradoxone.com>
2014-03-17 23:55:34 -0500

    Tenant validation update
    The tenant model will now set the connection for the internal validator
    to the current tenant connection.

--- src/Stillat/Common/Database/Tenant/Model.php
+++ src/Stillat/Common/Database/Tenant/Model.php
@@ -2,6 +2,7 @@
 
 use Stillat\Common\Database\Model as StillatModel;
 use Stillat\Common\Database\Tenant\TenantManager as Manager;
+use Illuminate\Support\Facades\Validator;
 
 abstract class Model extends StillatModel {
 
@@ -17,6 +18,37 @@ abstract class Model extends StillatModel {
 		$manager = Manager::instance();
 		$this->setConnection($manager->getCurrentConnection());
 		unset($manager);
+	}
+
+	/**
+     * Instatiates the validator used by the validation process, depending if the class is being used inside or
+     * outside of Laravel.
+     *
+     * @param $data
+     * @param $rules
+     * @param $customMessages
+     * @return \Illuminate\Validation\Validator
+     * @see Ardent::$externalValidator
+     */
+	protected static function makeValidator($data, $rules, $customMessages)
+	{
+
+		$manager = Manager::instance();
+		$tenantConnection = $manager->getCurrentConnection();
+		unset($manager);
+
+		if (self::$externalValidator)
+		{
+			$validator = self::$validationFactory->make($data, $rules, $customMessages);
+			$validator->getPresenceVerifier()->setConnection($tenantConnection);
+			return $validator;
+		}
+		else
+		{
+			$validator = Validator::make($data, $rules, $customMessages);
+			$validator->getPresenceVerifier()->setConnection($tenantConnection);
+			return $validator;
+		}
 	}
 
 }
