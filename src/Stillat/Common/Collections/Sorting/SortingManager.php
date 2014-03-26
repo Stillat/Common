9e9beb419e6157312ddee8c697a966a0fe308b21
Johnathon Koster <johnathon.koster@paradoxone.com>
2014-03-26 01:48:24 -0500

    implemented sorting manager class

--- src/Stillat/Common/Collections/Sorting/SortingManager.php
+++ src/Stillat/Common/Collections/Sorting/SortingManager.php
@@ -1,6 +1,25 @@
 <?php namespace Stillat\Common\Collections\Sorting;
 
+use Stillat\Common\Collections\Sorting\ArraySortingInterface;
+
 class SortingManager {
+
+	/**
+	 * The ArraySortingInterface implementation.
+	 *
+	 * @var \Stillat\Common\Collections\Sorting\ArraySortingInterface
+	 */
+	protected $driver;
+
+	/**
+	 * Returns an instance of SortingManager
+	 * 
+	 * @param ArraySortingInterface $sortingDriver
+	 */
+	public function __construct(ArraySortingInterface $sortingDriver)
+	{
+		$this->driver = $sortingDriver;
+	}
 
 	/**
 	 * Sorts an array in ascending order.
@@ -10,7 +29,7 @@ class SortingManager {
 	 */
 	public function asc(array $collection)
 	{
-
+		return $this->driver->sort($collection);
 	}
 
 	/**
@@ -21,7 +40,7 @@ class SortingManager {
 	 */
 	public function desc(array $collection)
 	{
-
+		return $this->driver->tros($collection);
 	}
 	
 }
