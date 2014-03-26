bf26de03372533d1c6f7423c46da36167f01825a
Johnathon Koster <johnathon.koster@paradoxone.com>
2014-03-26 01:48:34 -0500

    added sorting config file.

---
+++ src/config/sorting.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Default Sorting Driver
+	|--------------------------------------------------------------------------
+	|
+	| This option controls the sorting driver that will be utilized by the
+	| sorting manager.
+	|
+	| Supported: "bogo", "bubble", "cocktail", "gnome", "heap", "insertion",
+	|			 "merge", "native", "quick", "selection", "shell"
+	|
+	*/
+
+	'driver' => 'native',
+
+);
