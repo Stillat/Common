6072cd8cbe0e262d302e138119d85a4b6d22ce6a
Johnathon Koster <johnathon.koster@paradoxone.com>
2014-03-26 02:33:01 -0500

    sorting manager resolves driver

--- src/Stillat/Common/Collections/Sorting/SortingManager.php
+++ src/Stillat/Common/Collections/Sorting/SortingManager.php
@@ -1,8 +1,28 @@
 <?php namespace Stillat\Common\Collections\Sorting;
 
+use Stillat\Common\Exceptions\InvalidArgumentException;
 use Stillat\Common\Collections\Sorting\ArraySortingInterface;
 
 class SortingManager {
+
+	/**
+	 * Maps the sorting driver names to their class names.
+	 * 
+	 * @var array
+	 */
+	protected $driverClassMap = array(
+		'bogo' => '\Stillat\Common\Collections\Sorting\Drivers\BogoSorter',
+		'bubble' => '\Stillat\Common\Collections\Sorting\Drivers\BubbleSorter',
+		'cocktail' => '\Stillat\Common\Collections\Sorting\Drivers\CocktailSorter',
+		'gnome' => '\Stillat\Common\Collections\Sorting\Drivers\GnomeSorter',
+		'heap' => '\Stillat\Common\Collections\Sorting\Drivers\HeapSorter',
+		'insertion' => '\Stillat\Common\Collections\Sorting\Drivers\InsertionSorter',
+		'merge' => '\Stillat\Common\Collections\Sorting\Drivers\MergeSorter',
+		'native' => '\Stillat\Common\Collections\Sorting\Drivers\NativeQuickSorter',
+		'quick' => '\Stillat\Common\Collections\Sorting\Drivers\QuickSorter',
+		'selection' => '\Stillat\Common\Collections\Sorting\Drivers\SelectionSorter',
+		'shell' => '\Stillat\Common\Collections\Sorting\Drivers\ShellSorter',
+	);
 
 	/**
 	 * The ArraySortingInterface implementation.
@@ -14,11 +34,15 @@ class SortingManager {
 	/**
 	 * Returns an instance of SortingManager
 	 * 
-	 * @param ArraySortingInterface $sortingDriver
+	 * @param string $sortingDriver
 	 */
-	public function __construct(ArraySortingInterface $sortingDriver)
+	public function __construct($sortingDriver)
 	{
-		$this->driver = $sortingDriver;
+		if (!array_key_exists($sortingDriver, $this->driverClassMap))
+		{
+			throw new InvalidArgumentException("Sorting driver '{$sortingDriver}' is not supported.");
+		}
+		$this->driver = new $this->driverClassMap[$sortingDriver];
 	}
 
 	/**
