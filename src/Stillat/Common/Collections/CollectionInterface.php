a79b5e79fec6bee2926f903955c6b68fa11e12ce
Johnathon Koster <johnathon.koster@paradoxone.com>
2014-03-25 23:42:04 -0500

    Defined two new functions

--- src/Stillat/Common/Collections/CollectionInterface.php
+++ src/Stillat/Common/Collections/CollectionInterface.php
@@ -1,5 +1,23 @@
 <?php namespace Stillat\Common\Collections;
 
 interface CollectionInterface {
+
+	/**
+	 * Inserts a new item after an existing array element.
+	 * 
+	 * @param  array  $afterKey
+	 * @param  array  $newItem
+	 * @return void
+	 */
+	public function insertAfter($afterKey, array $newItem);
+
+	/**
+	 * Inserts an item before an existing array element.
+	 * 
+	 * @param  array  $beforeKey
+	 * @param  array  $newItem
+	 * @return void
+	 */
+	public function insertBefore($beforeKey, array $newItem);
 	
 }
