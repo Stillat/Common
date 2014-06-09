a8190543075ae1b32522c386d7b0663e49fd20d1
Johnathon Koster <johnathon.koster@paradoxone.com>
2014-06-09 14:53:52 -0500

    Updated config to match the supported drivers

--- src/config/sorting.php
+++ src/config/sorting.php
@@ -10,8 +10,7 @@ return array(
 	| This option controls the sorting driver that will be utilized by the
 	| sorting manager.
 	|
-	| Supported: "bogo", "bubble", "cocktail", "gnome", "heap", "insertion",
-	|			 "merge", "native", "quick", "selection", "shell"
+	| Supported: "native", "quick"
 	|
 	*/
 
