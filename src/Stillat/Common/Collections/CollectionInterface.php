c059164dcb2c324bc8569640e80992d728ca02d7
Johnathon Koster <johnathon.koster@paradoxone.com>
2014-06-08 22:08:57 -0500

    Updated collection API

--- src/Stillat/Common/Collections/CollectionInterface.php
+++ src/Stillat/Common/Collections/CollectionInterface.php
@@ -3,21 +3,37 @@
 interface CollectionInterface {
 
 	/**
-	 * Inserts a new item after an existing array element.
-	 * 
+	 * Adds a new item after an existing array element.
+	 *
+	 * @throws \Stillat\Common\Exceptions\InvalidArgumentException If an invalid $afterKey is specified
 	 * @param  array  $afterKey
 	 * @param  array  $newItem
 	 * @return void
 	 */
-	public function insertAfter($afterKey, array $newItem);
+	public function addAfter($afterKey, array $newItem);
 
 	/**
-	 * Inserts an item before an existing array element.
-	 * 
+	 * Adds an item before an existing array element.
+	 *
+	 * @throws \Stillat\Common\Exceptions\InvalidArgumentException If an invalid $beforeKey is specified
 	 * @param  array  $beforeKey
 	 * @param  array  $newItem
 	 * @return void
 	 */
-	public function insertBefore($beforeKey, array $newItem);
+	public function addBefore($beforeKey, array $newItem);
+
+	/**
+	 * Sort the collection in ascending order
+	 * 
+	 * @return \Stillat\Common\Collections\CollectionInterface
+	 */
+	public function asc();
+
+	/**
+	 * Sort the collection in descending order
+	 * 
+	 * @return \Stillat\Common\Collections\CollectionInterface
+	 */
+	public function desc();
 	
 }
