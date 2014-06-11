9070a500802306ec9c09ef8fd1d9a7a74cb124d7
Johnathon Koster <johnathon.koster@paradoxone.com>
2014-06-10 22:23:48 -0500

    Fixed a typo

--- src/Stillat/Common/Math/ExpressionEngines/BinaryCaclulatorExpressionEngine.php
+++ src/Stillat/Common/Math/ExpressionEngines/BinaryCaclulatorExpressionEngine.php
@@ -39,7 +39,8 @@ class BinaryCaclulatorExpressionEngine implements ExpressionEngineInterface {
 	public function abs($number)
 	{
 		return $this->withPrecision(abs($number));
-
+	}
+	
 	/**
 	 * {@inheritdoc}
 	 */
