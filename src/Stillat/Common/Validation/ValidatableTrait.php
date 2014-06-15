86fa90d037389254e54d9857eb5ce53976829246
Johnathon Koster <johnathon.koster@paradoxone.com>
2014-06-15 02:05:11 -0500

    Some updates to validatable trait
    Fixed a class name collision
    Increased comment code coverage
    Renamed 'validate' to 'isValid' -- reads nicer
    Added option for setting the presence verifier

--- src/Stillat/Common/Validation/ValidatableTrait.php
+++ src/Stillat/Common/Validation/ValidatableTrait.php
@@ -1,39 +1,93 @@
 <?php namespace Stillat\Common\Validation;
 
 use Illuminate\Validation\Validator;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Validator as ValidatorFacade;
+use Illuminate\Validation\PresenceVerifierInterface;
 
 trait ValidatableTrait {
 
 	/**
-	 * The validator implementation
+	 * The validator instance.
+	 *
+	 * @var \Illuminate\Validation\Validator
 	 */
 	protected $validatorInstance = null;
 
+	/**
+	 * The validation errors.
+	 *
+	 * @var \Illuminate\Support\MessageBag
+	 */
 	protected $validationErrors = null;
 
+	/**
+	 * The Presence Verifier implementation.
+	 *
+	 * @var \Illuminate\Validation\PresenceVerifierInterface
+	 */
+	protected $validationPresenceVerifier = null;
+
+	/**
+	 * Sets the Validator instance.
+	 *
+	 * @param  \Illuminate\Validation\Validator $validator
+	 * @return void
+	 */
 	public function setValidator(Validator $validator)
 	{
 		$this->validatorInstance = $validator;
 	}
 
+	/**
+	 * Gets the Validator instance.
+	 *
+	 * @return \Illuminate\Validation\Validator
+	 */
 	public function getValidator()
 	{
 		return $this->validatorInstance;
 	}
 
-	public function validate()
+	/**
+	 * Sets the Precense Verifier implementation.
+	 * 
+	 * @param  \Illuminate\Validation\PresenceVerifierInterface $presenceVerifier
+	 * @return void
+	 */
+	public function setPresenceVerifier(PresenceVerifierInterface $presenceVerifier)
+	{
+		$this->validationPresenceVerifier = $presenceVerifier;
+	}
+
+	/**
+	 * Determine if the data passes the validation rules.
+	 * 
+	 * @return bool
+	 */
+	public function isValid()
 	{
 		if ($this->getValidator() == null)
 		{
-			return $this->performValidation(Validator::make($this->getValidationData(), $this->getValidationRules(), $this->getValidationMessages()));
+			return $this->performValidation(ValidatorFacade::make($this->getValidationData(), $this->getValidationRules(), $this->getValidationMessages()));
 		}
 
 		return $this->performValidation($this->validatorInstance);
 	}
 
+	/**
+	 * Performs the actual validation process.
+	 * 
+	 * @param  \Illuminate\Validation\Validator $validator
+	 * @return bool
+	 */
 	private function performValidation(Validator $validator)
 	{
+
+		if ($this->validationPresenceVerifier !== null)
+		{
+			$validator->setPresenceVerifier($this->validationPresenceVerifier);
+		}
+
 		$passed = $validator->passes();
 
 		if (!$passed)
@@ -44,6 +98,11 @@ trait ValidatableTrait {
 		return $passed;
 	}
 
+	/**
+	 * Returns the validation errors.
+	 * 
+	 * @return \Illuminate\Support\MessageBag
+	 */
 	public function errors()
 	{
 		return $this->validationErrors;
@@ -64,13 +123,13 @@ trait ValidatableTrait {
 	 *
 	 * @return array
 	 */
-	abstract private function getValidationRules();
+	abstract function getValidationRules();
 
 	/**
 	 * Gets the validation data
 	 *
 	 * @return array
 	 */
-	abstract private function getValidationData();
+	abstract function getValidationData();
 
 }
