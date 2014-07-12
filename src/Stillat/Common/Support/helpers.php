07ad40cfe1dce72f4753a384ceb70d66afd55ba8
Johnathon Koster <johnathon.koster@paradoxone.com>
2014-07-12 13:22:28 -0500

    Added the array_key_exists_then_or helper method

--- src/Stillat/Common/Support/helpers.php
+++ src/Stillat/Common/Support/helpers.php
@@ -14,15 +14,41 @@ if (!function_exists('d'))
 	}
 }
 
+if (!function_exists('array_key_exists_then_or'))
+{
+	/**
+	 * Returns a different value when an array key exists, or doesn't.
+	 * 
+	 * 
+	 * @param  array            $array The array to check in.
+	 * @param  string           $check The key to check for.
+	 * @param  \Closure|mixed   $then  The value to return if the key exists.
+	 * @param  \Closure|mixed   $or    The value to return if the key does not exist.
+	 * @return mixed
+	 */
+	function array_key_exists_then_or(array $array, $check, $then = true, $or = false)
+	{
+		if (array_key_exists($check, $array))
+		{
+			if (is_object($then) and $then instanceof Closure) { return $then(); }
+
+			return $then;
+		}
+
+		if (is_object($or) and $or instanceof Closure) { return $or(); }
+
+		return $or;
+	}
+}
+
 if (!function_exists('if_null_then'))
 {
 	/**
 	 * Returns a value based on whether another value is null.
 	 *
-	 * Concerning arrays, if you need to check that a value has
-	 * been set (which is not necessarily the same is being
-	 * equal to null), consider using the `isset_or` function
-	 * instead.
+	 * If you need to check if a value exists in an array, such as a checkbox
+	 * input, consider using the `array_key_exists_then_or` for a simple inline
+	 * approach.
 	 * 
 	 * @param  mixed          $check The value to check for null.
 	 * @param  \Closure|mixed $then  The value to return if $check is null.
--- tests/SupportHelpersTest.php
+++ tests/SupportHelpersTest.php
@@ -4,6 +4,8 @@ class SupportHelpersTest extends PHPUnit_Framework_TestCase {
 
 	protected $testArray = array('fourty', 'two');
 
+	protected $testAssociativeArray = array('keyOne' => 'valueOne', 'keyTwo' => 'valueTwo');
+
 	public function testCanBeString()
 	{
 		$result = can_be_valid_string(null);
@@ -55,6 +57,44 @@ class SupportHelpersTest extends PHPUnit_Framework_TestCase {
 	{
 		$this->assertEquals('test', if_null_then(null, function(){
 			return 'test';
+		}));
+	}
+
+	public function testArrayKeyExistsThenOrThen()
+	{
+		$this->assertEquals(true, array_key_exists_then_or($this->testAssociativeArray, 'keyOne'));
+		$this->assertEquals(true, array_key_exists_then_or($this->testAssociativeArray, 'keyTwo'));
+	}
+
+	public function testArrayKeyExistsThenOrOr()
+	{
+		$this->assertEquals(false, array_key_exists_then_or($this->testAssociativeArray, 'keyThree'));
+		$this->assertEquals(false, array_key_exists_then_or($this->testAssociativeArray, 'keyFour'));
+	}
+
+	public function testArrayKeyExistsThenOrThenCustomOrThen()
+	{
+		$this->assertEquals('test', array_key_exists_then_or($this->testAssociativeArray, 'keyOne', 'test', 'test2'));
+		$this->assertEquals('test', array_key_exists_then_or($this->testAssociativeArray, 'keyTwo', 'test', 'test2'));
+
+
+		$this->assertEquals('test2', array_key_exists_then_or($this->testAssociativeArray, 'keyThree', 'test', 'test2'));
+		$this->assertEquals('test2', array_key_exists_then_or($this->testAssociativeArray, 'keyFour', 'test', 'test2'));
+	}
+
+	public function testArrayKeyExistsThenOrThenClosure()
+	{
+		$this->assertEquals('closure', array_key_exists_then_or($this->testAssociativeArray, 'keyOne', function()
+		{
+			return 'closure';
+		}));
+	}
+
+	public function testArrayKeyExistsThenOrOrClosure()
+	{
+		$this->assertEquals('closure', array_key_exists_then_or($this->testAssociativeArray, 'keyThree', true, function()
+		{
+			return 'closure';
 		}));
 	}
 
