0923cebcf3d40e821ff17dfbf69e1f87f93628b1
Johnathon Koster <johnathon.koster@paradoxone.com>
2015-11-22 17:59:41 -0600

    New formatting

--- src/Stillat/Common/Collections/Sorting/CollectionSortMap.php
+++ src/Stillat/Common/Collections/Sorting/CollectionSortMap.php
@@ -1,61 +1,63 @@
-<?php namespace Stillat\Common\Collections\Sorting;
+<?php
+
+namespace Stillat\Common\Collections\Sorting;
 
 use Stillat\Common\Exceptions\InvalidArgumentException;
 
-class CollectionSortMap {
+class CollectionSortMap
+{
 
-	protected $sortThresholds = array();
+    protected $sortThresholds = array();
 
-	/**
-	 * Adds a new sorting algorithm with the specified threshold
-	 *
-	 * @throws InvalidArgumentException if the $threshold is not an integer, or is not greater than 0
-	 * @param  int    $threshold The collection count threshold
-	 * @param  string $driver    The sorting driver to use at the threshold
-	 */
-	public function addThreshold($threshold, $driver)
-	{
-		if (!is_int($threshold) || $threshold <= 0)
-		{
-			throw new InvalidArgumentException("The threshold '{$threshold}' is invalid. The threshold must be an integer and greater than '0'.");
-		}
+    /**
+     * Adds a new sorting algorithm with the specified threshold
+     *
+     * @throws InvalidArgumentException if the $threshold is not an integer, or is not greater than 0
+     *
+     * @param  int    $threshold The collection count threshold
+     * @param  string $driver    The sorting driver to use at the threshold
+     */
+    public function addThreshold($threshold, $driver)
+    {
+        if (!is_int($threshold) || $threshold <= 0) {
+            throw new InvalidArgumentException("The threshold '{$threshold}' is invalid. The threshold must be an integer and greater than '0'.");
+        }
 
-		$this->sortThresholds[$threshold] = $driver;
-	}
+        $this->sortThresholds[$threshold] = $driver;
+    }
 
-	/**
-	 * Returns the collection map sort thresholds
-	 * 
-	 * @return array
-	 */
-	public function getThresholds()
-	{
-		return $this->sortThresholds;
-	}
+    /**
+     * Returns the collection map sort thresholds
+     *
+     * @return array
+     */
+    public function getThresholds()
+    {
+        return $this->sortThresholds;
+    }
 
-	/**
-	 * Determines the driver to use based on a collection's size
-	 * 
-	 * @param  int    $collectionCount The size of the collection
-	 * @return string The driver to use
-	 */
-	public function determineDriver($collectionCount)
-	{
+    /**
+     * Determines the driver to use based on a collection's size
+     *
+     * @param  int $collectionCount The size of the collection
+     *
+     * @return string The driver to use
+     */
+    public function determineDriver($collectionCount)
+    {
 
-		$determinedDriver = 'native';
-		
-		ksort($this->sortThresholds, SORT_NUMERIC);
+        $determinedDriver = 'native';
 
-		foreach ($this->sortThresholds as $threshold => $driver)
-		{
-			if ($collectionCount <= $threshold)
-			{
-				$determinedDriver = $driver;
-				break;
-			}
-		}
-		
-		return $determinedDriver;
-	}
+        ksort($this->sortThresholds, SORT_NUMERIC);
+
+        foreach ($this->sortThresholds as $threshold => $driver) {
+            if ($collectionCount <= $threshold) {
+                $determinedDriver = $driver;
+                break;
+            }
+        }
+
+        return $determinedDriver;
+    }
 
 }
