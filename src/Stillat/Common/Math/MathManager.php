b4cf8da4742140607af75defc19c95ce87b04429
Johnathon Koster <johnathon.koster@paradoxone.com>
2015-11-24 20:32:19 -0600

    Simplified manager construction

--- src/Stillat/Common/Math/MathManager.php
+++ src/Stillat/Common/Math/MathManager.php
@@ -1,421 +1,405 @@
-<?php namespace Stillat\Common\Math;
+<?php
 
-use Stillat\Common\Exceptions\InvalidArgumentException;
-use Stillat\Common\Math\ExpressionEngines\ExpressionEngineInterface;
+namespace Stillat\Common\Math;
 
-class MathManager {
-	
-	/**
-	 * The expression engine implementation.
-	 * 
-	 * @var \Stillat\Common\Math\ExpressionEngines\ExpressionEngineInterface
-	 */
-	protected $expressionEngine = null;
+use Stillat\Common\Contracts\Math\ExpressionEngineInterface;
 
-	/**
-	 * Maps the math expression engine names to their class names.
-	 * 
-	 * @var array
-	 */
-	protected $driverClassMap = array(
-		'bc' => '\Stillat\Common\Math\ExpressionEngines\BinaryCalculatorExpressionEngine',
-		'native' => '\Stillat\Common\Math\ExpressionEngines\NativeExpressionEngine',
-		);
+class MathManager
+{
 
     /**
-     * Returns an instance of MathManager
+     *  The expression engine implementation.
      *
-     * @param       $mathDriver
-     * @param array $additionalDrivers An array of additional sorting drivers.
+     * @var \Stillat\Common\Contracts\Math\ExpressionEngineInterface
      */
-	public function __construct($mathDriver, array $additionalDrivers = array())
-	{
-        if (count($additionalDrivers) > 0)
-        {
-            array_merge($this->driverClassMap, $additionalDrivers);
-        }
+    protected $expressionEngine = null;
 
-        $this->makeDriver($mathDriver);
-	}
-
-    /**
-     * Attempts to make an instance of an expression engine.
-     *
-     * @param  string $driverName The driver name.
-     * @return \Stillat\Common\Math\ExpressionEngines\ExpressionEngineInterface
-     * @throws InvalidArgumentException
-     */
-    private function makeDriver($driverName)
+    public function __construct(ExpressionEngineInterface $expressionEngine)
     {
-        if (is_object($driverName) && $driverName instanceof ExpressionEngineInterface)
-        {
-            $this->expressionEngine = $driverName;
-            return;
-        }
-        else
-        {
-            if (is_string($driverName) and array_key_exists($driverName, $this->driverClassMap) == false)
-            {
-                throw new InvalidArgumentException("Math expression engine '{$driverName}' is not supported.");
-            }
-            else
-            {
-                $this->expressionEngine = new $this->driverClassMap[$driverName];
-            }
-        }
+        $this->expressionEngine = $expressionEngine;
     }
 
-	/**
-	 * Returns the ExpressionEngineInterface implementation
-	 * 
-	 * @return \Stillat\Common\Math\ExpressionEngines\ExpressionEngineInterface
-	 */
-	public function getDriver()
-	{
-		return $this->expressionEngine;
-	}
+    /**
+     * Returns the ExpressionEngineInterface implementation
+     *
+     * @return \Stillat\Common\Contracts\Math\ExpressionEngineInterface
+     */
+    public function getExpressionEngine()
+    {
+        return $this->expressionEngine;
+    }
 
-	/**
-	 * Sets the precision to use in calculations
-	 * 
-	 * @param int $precision The precision to use
+    /**
+     * Sets the precision to use in calculations
+     *
+     * @param int $precision The precision to use
+     *
      * @return MathManager
-	 */
-	public function setPrecision($precision)
-	{
-		$this->expressionEngine->setPrecision($precision);
+     */
+    public function setPrecision($precision)
+    {
+        $this->expressionEngine->setPrecision($precision);
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Returns the absolute value of a number.
-	 *
-	 * @param  float $number
-	 * @return float|int
-	 */
-	public function abs($number)
-	{
-		return $this->expressionEngine->abs($number);
-	}
+    /**
+     * Returns the absolute value of a number.
+     *
+     * @param  float $number
+     *
+     * @return float|int
+     */
+    public function abs($number)
+    {
+        return $this->expressionEngine->abs($number);
+    }
 
-	/**
-	 * Returns the arc cosine of a number.
-	 *
-	 * @param  float $number
-	 * @return float The arc cosine of a number in radians.
-	 */
-	public function acos($number)
-	{
-		return $this->expressionEngine->acos($number);
-	}
+    /**
+     * Returns the arc cosine of a number.
+     *
+     * @param  float $number
+     *
+     * @return float The arc cosine of a number in radians.
+     */
+    public function acos($number)
+    {
+        return $this->expressionEngine->acos($number);
+    }
 
-	/**
-	 * Returns the arc sine of a number.
-	 *
-	 * @param  float $number
-	 * @return float The arc sine of a number in radians.
-	 */
-	public function asin($number)
-	{
-		return $this->expressionEngine->asin($number);
-	}
+    /**
+     * Returns the arc sine of a number.
+     *
+     * @param  float $number
+     *
+     * @return float The arc sine of a number in radians.
+     */
+    public function asin($number)
+    {
+        return $this->expressionEngine->asin($number);
+    }
 
-	/**
-	 * Returns the arc tangent of a number.
-	 *
-	 * @param  float $number
-	 * @return float The arc tangent of a number in radians.
-	 */
-	public function atan($number)
-	{
-		return $this->expressionEngine->atan($number);
-	}
+    /**
+     * Returns the arc tangent of a number.
+     *
+     * @param  float $number
+     *
+     * @return float The arc tangent of a number in radians.
+     */
+    public function atan($number)
+    {
+        return $this->expressionEngine->atan($number);
+    }
 
-	/**
-	 * Calculates the arc tangent of two variables.
-	 *
-	 * @param  float $x
-	 * @param  float $y
-	 * @return float
-	 */
-	public function atan2($x, $y)
-	{
-		return $this->expressionEngine->atan2($x, $y);
-	}
+    /**
+     * Calculates the arc tangent of two variables.
+     *
+     * @param  float $x
+     * @param  float $y
+     *
+     * @return float
+     */
+    public function atan2($x, $y)
+    {
+        return $this->expressionEngine->atan2($x, $y);
+    }
 
-	/**
-	 * Returns the next highest integer of a number.
-	 *
+    /**
+     * Returns the next highest integer of a number.
+     *
      * @param  number $number
-	 * @return int
-	 */
-	public function ceiling($number)
-	{
-		return $this->expressionEngine->ceiling($number);
-	}
+     *
+     * @return int
+     */
+    public function ceiling($number)
+    {
+        return $this->expressionEngine->ceiling($number);
+    }
 
-	/**
-	 * Returns the cosine of the specified angle.
-	 *
-	 * @param  float $angle
-	 * @return float
-	 */
-	public function cos($angle)
-	{
-		return $this->expressionEngine->cos($angle);
-	}
+    /**
+     * Returns the cosine of the specified angle.
+     *
+     * @param  float $angle
+     *
+     * @return float
+     */
+    public function cos($angle)
+    {
+        return $this->expressionEngine->cos($angle);
+    }
 
-	/**
-	 * Returns the hyperbolic cosine of the angle.
-	 *
-	 * @param  float $angle
-	 * @return float
-	 */
-	public function cosh($angle)
-	{
-		return $this->expressionEngine->cosh($angle);
-	}
+    /**
+     * Returns the hyperbolic cosine of the angle.
+     *
+     * @param  float $angle
+     *
+     * @return float
+     */
+    public function cosh($angle)
+    {
+        return $this->expressionEngine->cosh($angle);
+    }
 
-	/**
-	 * Returns e raised to a given power.
-	 *
-	 * @param  float $number
-	 * @return double
-	 */
-	public function exp($number)
-	{
-		return $this->expressionEngine->exp($number);
-	}
+    /**
+     * Returns e raised to a given power.
+     *
+     * @param  float $number
+     *
+     * @return double
+     */
+    public function exp($number)
+    {
+        return $this->expressionEngine->exp($number);
+    }
 
-	/**
-	 * Returns the next lowest integer of a number.
-	 *
-	 * @param  float $number
-	 * @return int
-	 */
-	public function floor($number)
-	{
-		return $this->expressionEngine->floor($number);
-	}
+    /**
+     * Returns the next lowest integer of a number.
+     *
+     * @param  float $number
+     *
+     * @return int
+     */
+    public function floor($number)
+    {
+        return $this->expressionEngine->floor($number);
+    }
 
-	/**
-	 * Returns the logarithm of a number in a specified base.
-	 *
-	 * @param  double $number
-	 * @param  double $base optional
-	 * @return double
-	 */
-	public function log($number, $base = M_E)
-	{
-		return $this->expressionEngine->log($number, $base);
-	}
+    /**
+     * Returns the logarithm of a number in a specified base.
+     *
+     * @param  double $number
+     * @param  double $base optional
+     *
+     * @return double
+     */
+    public function log($number, $base = M_E)
+    {
+        return $this->expressionEngine->log($number, $base);
+    }
 
-	/**
-	 * Returns the base-10 logarithm of a number.
-	 *
-	 * @param  double $number
-	 * @return double
-	 */
-	public function log10($number)
-	{
-		return $this->expressionEngine->log10($number);
-	}
+    /**
+     * Returns the base-10 logarithm of a number.
+     *
+     * @param  double $number
+     *
+     * @return double
+     */
+    public function log10($number)
+    {
+        return $this->expressionEngine->log10($number);
+    }
 
-	/**
-	 * Returns the highest value in the array of numbers.
-	 *
-	 * @param  array $numbers
-	 * @return number|mixed
-	 */
-	public function max(array $numbers)
-	{
-		return $this->expressionEngine->max($numbers);
-	}
+    /**
+     * Returns the highest value in the array of numbers.
+     *
+     * @param  array $numbers
+     *
+     * @return number|mixed
+     */
+    public function max(array $numbers)
+    {
+        return $this->expressionEngine->max($numbers);
+    }
 
-	/**
-	 * Returns the lowest value in the array of numbers.
-	 *
-	 * @param  array $numbers
-	 * @return number|mixed
-	 */
-	public function min(array $numbers)
-	{
-		return $this->expressionEngine->min($numbers);
-	}
+    /**
+     * Returns the lowest value in the array of numbers.
+     *
+     * @param  array $numbers
+     *
+     * @return number|mixed
+     */
+    public function min(array $numbers)
+    {
+        return $this->expressionEngine->min($numbers);
+    }
 
-	/**
-	 * Returns a base number raised to an exponent.
-	 *
-	 * @param  $base     number
-	 * @param  $exponent number
-	 * @return float
-	 */
-	public function pow($base, $exponent)
-	{
-		return $this->expressionEngine->pow($base, $exponent);
+    /**
+     * Returns a base number raised to an exponent.
+     *
+     * @param  $base     number
+     * @param  $exponent number
+     *
+     * @return float
+     */
+    public function pow($base, $exponent)
+    {
+        return $this->expressionEngine->pow($base, $exponent);
 
-	}
+    }
 
-	/**
-	 * Rounds a number to the nearest value.
-	 *
-	 * @param float $number
-	 * @param int   $precision optional The number of digits to round to.
-	 * @param int   $mode      optional The rounding mode.
-	 * @return number|mixed
-	 */
-	public function round($number, $precision = 0, $mode = PHP_ROUND_HALF_UP)
-	{
-		return $this->expressionEngine->round($number, $precision, $mode);
-	}
+    /**
+     * Rounds a number to the nearest value.
+     *
+     * @param float $number
+     * @param int   $precision optional The number of digits to round to.
+     * @param int   $mode      optional The rounding mode.
+     *
+     * @return number|mixed
+     */
+    public function round($number, $precision = 0, $mode = PHP_ROUND_HALF_UP)
+    {
+        return $this->expressionEngine->round($number, $precision, $mode);
+    }
 
-	/**
-	 * Returns a value indicating the sign of a number.
-	 *
-	 * @param  number|mixed $number
-	 * @return int
-	 */
-	public function sign($number)
-	{
-		return $this->expressionEngine->sign($number);
-	}
+    /**
+     * Returns a value indicating the sign of a number.
+     *
+     * @param  number|mixed $number
+     *
+     * @return int
+     */
+    public function sign($number)
+    {
+        return $this->expressionEngine->sign($number);
+    }
 
-	/**
-	 * Returns the sine of the given angle.
-	 *
-	 * @param  number|mixed $angle The angle in radians.
-	 * @return number|mixed 
-	 */
-	public function sin($angle)
-	{
-		return $this->expressionEngine->sin($angle);
-	}
+    /**
+     * Returns the sine of the given angle.
+     *
+     * @param  number|mixed $angle The angle in radians.
+     *
+     * @return number|mixed
+     */
+    public function sin($angle)
+    {
+        return $this->expressionEngine->sin($angle);
+    }
 
-	/**
-	 * Returns the hyperbolic sine of of the angle.
-	 *
-	 * @param  number|mixed $angle
-	 * @return number|mixed
-	 */
-	public function sinh($angle)
-	{
-		return $this->expressionEngine->sinh($angle);
-	}
+    /**
+     * Returns the hyperbolic sine of of the angle.
+     *
+     * @param  number|mixed $angle
+     *
+     * @return number|mixed
+     */
+    public function sinh($angle)
+    {
+        return $this->expressionEngine->sinh($angle);
+    }
 
-	/**
-	 * Returns the square root of a given number.
-	 *
-	 * @param  number|mixed $number
-	 * @return number|mixed
-	 */
-	public function sqrt($number)
-	{
-		return $this->expressionEngine->sqrt($number);
-	}
+    /**
+     * Returns the square root of a given number.
+     *
+     * @param  number|mixed $number
+     *
+     * @return number|mixed
+     */
+    public function sqrt($number)
+    {
+        return $this->expressionEngine->sqrt($number);
+    }
 
-	/**
-	 * Returns the tangent of a specified angle.
-	 *
-	 * @param  number|mixed $angle The angle in radians.
-	 * @return number|mixed
-	 */
-	public function tan($angle)
-	{
-		return $this->expressionEngine->tan($angle);
-	}
+    /**
+     * Returns the tangent of a specified angle.
+     *
+     * @param  number|mixed $angle The angle in radians.
+     *
+     * @return number|mixed
+     */
+    public function tan($angle)
+    {
+        return $this->expressionEngine->tan($angle);
+    }
 
-	/**
-	 * Returns the hyperbolic tangent of an angle.
-	 *
-	 * @param  number|mixed $angle
-	 * @return number|mixed
-	 */
-	public function tanh($angle)
-	{
-		return $this->expressionEngine->tanh($angle);
-	}
+    /**
+     * Returns the hyperbolic tangent of an angle.
+     *
+     * @param  number|mixed $angle
+     *
+     * @return number|mixed
+     */
+    public function tanh($angle)
+    {
+        return $this->expressionEngine->tanh($angle);
+    }
 
-	/**
-	 * Returns the integral part of a given number.
-	 *
-	 * @param  float $number
-	 * @return int|0 on failure
-	 */
-	public function truncate($number)
-	{
-		return $this->expressionEngine->truncate($number);
-	}
+    /**
+     * Returns the integral part of a given number.
+     *
+     * @param  float $number
+     *
+     * @return int|0 on failure
+     */
+    public function truncate($number)
+    {
+        return $this->expressionEngine->truncate($number);
+    }
 
-	/**
-	 * Returns the sum of two numbers.
-	 *
-	 * @param  number|mixed $numberOne
-	 * @param  number|mixed $numberTwo
-	 * @return number|mixed
-	 */
-	public function add($numberOne, $numberTwo)
-	{
-		return $this->expressionEngine->add($numberOne, $numberTwo);
-	}
+    /**
+     * Returns the sum of two numbers.
+     *
+     * @param  number|mixed $numberOne
+     * @param  number|mixed $numberTwo
+     *
+     * @return number|mixed
+     */
+    public function add($numberOne, $numberTwo)
+    {
+        return $this->expressionEngine->add($numberOne, $numberTwo);
+    }
 
-	/**
-	 * Returns the difference of two numbers.
-	 *
-	 * @param  number|mixed $numberOne
-	 * @param  number|mixed $numberTwo
-	 * @return number|mixed
-	 */
-	public function subtract($numberOne, $numberTwo)
-	{
-		return $this->expressionEngine->subtract($numberOne, $numberTwo);
-	}
+    /**
+     * Returns the difference of two numbers.
+     *
+     * @param  number|mixed $numberOne
+     * @param  number|mixed $numberTwo
+     *
+     * @return number|mixed
+     */
+    public function subtract($numberOne, $numberTwo)
+    {
+        return $this->expressionEngine->subtract($numberOne, $numberTwo);
+    }
 
-	/**
-	 * Returns the product of two numbers.
-	 *
-	 * @param  number|mixed $numberOne
-	 * @param  number|mixed $numberTwo
-	 * @return number|mixed
-	 */
-	public function multiply($numberOne, $numberTwo)
-	{
-		return $this->expressionEngine->multiply($numberOne, $numberTwo);
-	}
+    /**
+     * Returns the product of two numbers.
+     *
+     * @param  number|mixed $numberOne
+     * @param  number|mixed $numberTwo
+     *
+     * @return number|mixed
+     */
+    public function multiply($numberOne, $numberTwo)
+    {
+        return $this->expressionEngine->multiply($numberOne, $numberTwo);
+    }
 
-	/**
-	 * Returns the quotient of two numbers.
-	 *
-	 * @param  number|mixed $numberOne
-	 * @param  number|mixed $numberTwo
-	 * @return number|mixed
-	 */
-	public function divide($numberOne, $numberTwo)
-	{
-		return $this->expressionEngine->divide($numberOne, $numberTwo);
-	}
+    /**
+     * Returns the quotient of two numbers.
+     *
+     * @param  number|mixed $numberOne
+     * @param  number|mixed $numberTwo
+     *
+     * @return number|mixed
+     */
+    public function divide($numberOne, $numberTwo)
+    {
+        return $this->expressionEngine->divide($numberOne, $numberTwo);
+    }
 
-	/**
-	 * Returns the remainder of two numbers.
-	 *
-	 * @param  number|mixed $numberOne
-	 * @param  number|mixed $numberTwo
-	 * @return number|mixed
-	 */
-	public function mod($numberOne, $numberTwo)
-	{
-		return $this->expressionEngine->mod($numberOne, $numberTwo);
-	}
+    /**
+     * Returns the remainder of two numbers.
+     *
+     * @param  number|mixed $numberOne
+     * @param  number|mixed $numberTwo
+     *
+     * @return number|mixed
+     */
+    public function mod($numberOne, $numberTwo)
+    {
+        return $this->expressionEngine->mod($numberOne, $numberTwo);
+    }
 
-	/**
-	 * Calculates the factorial of a number.
-	 *
-	 * @param  number|mixed $number
-	 * @return number|mixed
-	 */
-	public function factorial($number)
-	{
-		return $this->expressionEngine->factorial($number);
-	}
+    /**
+     * Calculates the factorial of a number.
+     *
+     * @param  number|mixed $number
+     *
+     * @return number|mixed
+     */
+    public function factorial($number)
+    {
+        return $this->expressionEngine->factorial($number);
+    }
 
 }
