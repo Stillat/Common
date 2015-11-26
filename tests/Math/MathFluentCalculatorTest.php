1f50a64557e1271f4dc7091086ebe7298f7def6d
Johnathon Koster <johnathon.koster@paradoxone.com>
2015-11-26 12:50:41 -0600

    Formatting

--- tests/Math/MathFluentCalculatorTest.php
+++ tests/Math/MathFluentCalculatorTest.php
@@ -40,7 +40,7 @@ class MathFluentCalculatorTest extends PHPUnit_Framework_TestCase
         $this->calculator->add(10)->add(10);
         $this->assertEquals(20, $this->calculator->get());
 
-        $this->calculator->with('sales')->add([10,10, 10]);
+        $this->calculator->with('sales')->add([10, 10, 10]);
         $this->assertEquals(30, $this->calculator->get('sales'));
     }
 
@@ -49,7 +49,7 @@ class MathFluentCalculatorTest extends PHPUnit_Framework_TestCase
         $this->calculator->set(10)->subtract(10);
         $this->assertEquals(0, $this->calculator->get());
 
-        $this->calculator->add(20)->subtract([10,5,5]);
+        $this->calculator->add(20)->subtract([10, 5, 5]);
         $this->assertEquals(0, $this->calculator->get());
     }
 
@@ -61,7 +61,7 @@ class MathFluentCalculatorTest extends PHPUnit_Framework_TestCase
         $this->calculator->set(0)->multiply(10);
         $this->assertEquals(0, $this->calculator->get());
 
-        $this->calculator->set(1)->multiply([5,5]);
+        $this->calculator->set(1)->multiply([5, 5]);
         $this->assertEquals(25, $this->calculator->get());
     }
 
@@ -70,7 +70,7 @@ class MathFluentCalculatorTest extends PHPUnit_Framework_TestCase
         $this->calculator->set(25)->divide(5);
         $this->assertEquals(5, $this->calculator->get());
 
-        $this->calculator->set(8)->divide([2,2]);
+        $this->calculator->set(8)->divide([2, 2]);
         $this->assertEquals(2, $this->calculator->get());
     }
 
@@ -84,22 +84,22 @@ class MathFluentCalculatorTest extends PHPUnit_Framework_TestCase
 
     public function testFluentCalculatorGrouping()
     {
-        $this->calculator->add(2)->subtract(4)->add()->group(function(FluentCalculator $calc) {
+        $this->calculator->add(2)->subtract(4)->add()->group(function (FluentCalculator $calc) {
             $calc->add(3)->multiply(3);
         });
         $this->assertEquals(7, $this->calculator->get());
 
         /**
-         * Test 2 + (3 - 3 + (33 - 23 + 2 * (9 /3 ) ) )
+         * Test 2 + (3 - 3 + (33 - 23 + (2 * (9 / 3))))
          */
-        $this->calculator->set(2)->add()->group(function(FluentCalculator $calc) {
-           $calc->add(3)->subtract(3)->add()->group(function(FluentCalculator $calc) {
-               $calc->add(33)->subtract(23)->add()->group(function(FluentCalculator $calc) {
-                   $calc->set(2)->multiply()->group(function(FluentCalculator $calc) {
-                       $calc->set(9)->divide(3);
-                 });
-              });
-           });
+        $this->calculator->set(2)->add()->group(function (FluentCalculator $calc) {
+            $calc->add(3)->subtract(3)->add()->group(function (FluentCalculator $calc) {
+                $calc->add(33)->subtract(23)->add()->group(function (FluentCalculator $calc) {
+                    $calc->set(2)->multiply()->group(function (FluentCalculator $calc) {
+                        $calc->set(9)->divide(3);
+                    });
+                });
+            });
         });
         $this->assertEquals(18, $this->calculator->get());
     }
